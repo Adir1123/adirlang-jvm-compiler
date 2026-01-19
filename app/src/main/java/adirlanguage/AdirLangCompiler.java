ff0d752d7cfc2b37ad2f74b2ba103dd11b0a6587
Adir Gabay <adirgabay9@gmail.com>
2026-01-19 18:21:07 +0200

    Compiler baseline before lexer and semantic analysis

---
+++ app/src/main/java/adirlanguage/AdirLangCompiler.java
@@ -0,0 +1,86 @@
+package adirlanguage;
+
+import ast.*;
+import codegen.CodeGen;
+import parser.Parser;
+
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+// A compiler written in Java that translates a custom language into JVM bytecode (.class files)
+public class AdirLangCompiler implements Opcodes {
+
+
+    // =========================
+    // Main: compile program.adir into Main.class
+    // =========================
+
+    public static void main(String[] args) throws Exception {
+
+
+        // 1. Read source file - My language
+        var in = AdirLangCompiler.class.getResourceAsStream("/program.adir");
+
+        if (in == null) {
+            throw new RuntimeException("program.adir not found in resources");
+        }
+        
+        String source = new String(in.readAllBytes()).trim();
+        Parser parser = new Parser(source);
+        List<Stmt> program = parser.parseProgram();
+        
+
+        // AdirProgram.class
+        ClassWriter cw = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
+
+        // public class AdirProgram extends Object
+        cw.visit(V17,ACC_PUBLIC, "adirlanguage/runtime/ProgramAdir", null, "java/lang/Object", null);
+
+
+        // constructor: public AdirProgram() { super(); }
+        MethodVisitor ctor = cw.visitMethod(ACC_PUBLIC, "<init>", "()V", null, null);
+        ctor.visitCode();
+        ctor.visitVarInsn(ALOAD, 0);
+        ctor.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+        ctor.visitInsn(RETURN);
+        ctor.visitMaxs(0, 0);
+        ctor.visitEnd();
+
+        // public static void main(String[] args) 
+        MethodVisitor mv = cw.visitMethod(ACC_PUBLIC | ACC_STATIC, "main", "([Ljava/lang/String;)V", null, null);
+        mv.visitCode();
+
+        
+      
+        Map<String, Integer> locals = new HashMap<>();
+        int[] nextLocalIndex = new int[]{1}; // slot 0 is for args
+
+
+        
+        for (Stmt stmt : program) {
+            CodeGen.emitStmt(mv, stmt, locals, nextLocalIndex);
+        }
+
+        mv.visitInsn(RETURN);
+        mv.visitMaxs(0, 0);
+        mv.visitEnd();
+
+        cw.visitEnd();
+
+        // Write to AdirProgram.class
+        Path out = Path.of("adirlanguage", "runtime", "ProgramAdir.class");
+        Files.createDirectories(out.getParent());
+        Files.write(out, cw.toByteArray());
+
+
+        System.out.println("Compiled AdirLang source (program.adir) to JVM bytecode (AdirProgram.class) inside app/build/run");
+    }
+}
---
+++ app/src/main/java/codegen/CodeGen.java
@@ -0,0 +1,108 @@
+package codegen;
+
+import ast.*;
+import org.objectweb.asm.MethodVisitor;
+import org.objectweb.asm.Opcodes;
+
+import java.util.Map;
+
+public class CodeGen implements Opcodes {
+
+    // ============================
+    // Helper — pushes an integer constant onto the operand stack
+    // Chooses the most efficient instruction based on value range
+    // ============================
+
+    static void pushInt(MethodVisitor mv, int value) {
+
+        
+        if (value >= -1 && value <= 5) {
+            mv.visitInsn(ICONST_0 + value);
+            return;
+        }
+
+        
+        if (value >= -128 && value <= 127) {
+            mv.visitIntInsn(BIPUSH, value);
+            return;
+        }
+
+        
+        if (value >= -32768 && value <= 32767) {
+            mv.visitIntInsn(SIPUSH, value);
+            return;
+        }
+
+        
+        mv.visitLdcInsn(value);
+    }
+
+
+    // ============================
+    // Codegen — converts Expr nodes into JVM bytecode instructions
+    // ============================
+
+    public static void emitExpr(MethodVisitor mv, Expr expr, Map<String, Integer> locals) {
+
+        
+        if (expr instanceof NumberExpr n) {
+            pushInt(mv, n.value);
+            return;
+        }
+
+        
+        if (expr instanceof VarExpr v) {
+            Integer slot = locals.get(v.name);
+            if (slot == null) throw new RuntimeException("Undefined variable: " + v.name);
+            mv.visitVarInsn(ILOAD, slot);
+            return;
+        }
+
+        // Binary: emit left, emit right, then op
+        if (expr instanceof BinaryExpr b) {
+            emitExpr(mv, b.left, locals);
+            emitExpr(mv, b.right, locals);
+
+            if (b.op == '+') {
+                mv.visitInsn(IADD);
+                return;
+            }
+
+            if (b.op == '*') {
+                mv.visitInsn(IMUL);
+                return;
+            }
+
+            throw new RuntimeException("Unknown operator: " + b.op);
+        }
+
+        throw new RuntimeException("Unknown Expr node");
+    }
+
+
+    // ============================
+    // Codegen — converts Stmt nodes into JVM bytecode instructions
+    // ============================
+
+    public static void emitStmt(MethodVisitor mv, Stmt stmt, Map<String, Integer> locals, int[] nextLocalIndex) {
+
+        // let x = expr;
+        if (stmt instanceof LetStmt s) {
+            emitExpr(mv, s.value, locals);
+
+            int slot = locals.computeIfAbsent(s.name, k -> nextLocalIndex[0]++);
+            mv.visitVarInsn(ISTORE, slot);
+            return;
+        }
+
+        // print expr;
+        if (stmt instanceof PrintStmt s) {
+            mv.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"); // System.out
+            emitExpr(mv, s.expr, locals);
+            mv.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false); // println(int)
+            return;
+        }
+
+        throw new RuntimeException("Unknown Stmt node");
+    }
+}
